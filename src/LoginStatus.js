a6962cd96c0069a3ab8f9223587b6ae93318f2b2
cloedy <skywangqj@gmail.com>
2019-05-23 08:14:58 +0800

    add

--- src/LoginStatus.js
+++ src/LoginStatus.js
@@ -1,30 +1,57 @@
 import React, {Component} from 'react';
-import {Button} from 'antd';
+import {Button, Dropdown, Menu, Icon} from 'antd';
 
+const menu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="/login">
+          login
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="/logout">
+          logout
+        </a>
+      </Menu.Item>
+    </Menu>
+);
 
 class LoginStatus extends Component {
-    state = {
-        isLogin: false
+  constructor() {
+    super();
+    this.state = {
+      isLogin: false
     };
+  }
 
-    change = () => {
-        this.setState( {
-            isLogin: !this.state.isLogin
-        });
-    };
-    render() {
+  componentDidMount() {
+    this.setState({
+      log: setInterval(() => console.info((new Date()).toLocaleDateString()), 1000)
+    })
+  }
 
-        return (
-            <div>
-                {
-                    this.state.isLogin ? <span><a>logged</a></span>
-                    : <a href="#">login</a>
-                }
-                <Button type={"default"} onClick={this.change}>change</Button>
+  change = () => {
+    this.setState({
+      isLogin: !this.state.isLogin
+    });
+  };
 
-            </div>
-        );
-    }
+
+  render() {
+
+    return (
+        <div style={{float: 'right'}}>
+          <Dropdown overlay={menu}>
+            <a className="ant-dropdown-link" href="#">
+              Hover me <Icon type="down" />
+            </a>
+          </Dropdown>
+
+          <Button type={"default"} onClick={this.change}>change</Button>
+
+        </div>
+    );
+  }
 }
 
 export default LoginStatus;
